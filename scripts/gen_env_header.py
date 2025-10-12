65d1cd533c48e75b5774508b3517f80e304cac0f
Henrique Honorio da Silva <contato@henriquesilva.dev>
2025-10-12 09:03:21 -0300

    feat: add novas envs

--- scripts/gen_env_header.py
+++ scripts/gen_env_header.py
@@ -8,10 +8,18 @@ content = f"""#pragma once
 #define WIFI_SSID "{os.getenv('WIFI_SSID')}"
 #define WIFI_PASS "{os.getenv('WIFI_PASS')}"
 
-#define API_KEY "{os.getenv('FIREBASE_API_KEY')}"
-#define PROJECT_ID "{os.getenv('FIREBASE_PROJECT_ID')}"
-#define USER_EMAIL "{os.getenv('FIREBASE_USER_EMAIL')}"
+#define API_KEY       "{os.getenv('FIREBASE_API_KEY')}"
+#define PROJECT_ID    "{os.getenv('FIREBASE_PROJECT_ID')}"
+#define USER_EMAIL    "{os.getenv('FIREBASE_USER_EMAIL')}"
 #define USER_PASSWORD "{os.getenv('FIREBASE_USER_PASS')}"
+#define FIRESTORE_DB  "{os.getenv('FIREBASE_FIRESTORE_DB')}"
+
+#define OM_LAT        "{os.getenv('OM_LAT')}"
+#define OM_LON        "{os.getenv('OM_LON')}"
+#define OM_LANG       "{os.getenv('OM_LANG')}"
+#define OM_UNITS      "{os.getenv('OM_UNITS')}"
+
+#define PUMP_ENABLE_DEFAULT "{os.getenv('PUMP_ENABLE_DEFAULT', 'false')}"
 """
 
 os.makedirs("firmware/include", exist_ok=True)
